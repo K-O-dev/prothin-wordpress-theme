dc7f0b22d8eff0ad6e54ec31527beb4f69b83a93
K-O-dev <solon.kimorven@gmail.com>
2024-10-22 06:07:01 -0400

    put all necessary parts of the single.html except the sidebar

--- script.js
+++ script.js
@@ -1,38 +1,46 @@
-const inputField = document.querySelector('.search-section input');
-const clearIcon = document.querySelector('.clear-icon');
+document.addEventListener("DOMContentLoaded", function() {
+  const inputField = document.querySelector('.search-section input');
+  const clearIcon = document.querySelector('.clear-icon');
 
-// Function to toggle the visibility of the clear icon
-function toggleClearIcon() {
-  clearIcon.style.display = inputField.value ? 'block' : 'none';
-}
+  // Check if inputField and clearIcon exist
+  if (inputField && clearIcon) {
+      // Function to toggle the visibility of the clear icon
+      function toggleClearIcon() {
+          clearIcon.style.display = inputField.value ? 'block' : 'none';
+      }
 
-// Show/hide clear icon based on input value
-inputField.addEventListener('input', toggleClearIcon);
+      // Show/hide clear icon based on input value
+      inputField.addEventListener('input', toggleClearIcon);
 
-// Clear input field when X icon is clicked
-clearIcon.addEventListener('click', function() {
-  inputField.value = '';
-  toggleClearIcon(); 
-});
+      // Clear input field when X icon is clicked
+      clearIcon.addEventListener('click', function() {
+          inputField.value = '';
+          toggleClearIcon(); 
+      });
+  }
 
-document.addEventListener('click', function (event) {
+  // Handle nav menu toggling
   const navMenu = document.getElementById('nav-menu');
   const hamburger = document.getElementById('hamburger');
   const body = document.body; 
 
-  // Check if the click was on the hamburger icon
-  if (event.target === hamburger) {
-    navMenu.classList.toggle('show');
-    if (navMenu.classList.contains('show')) {
-      body.style.overflow = 'hidden';
-    } else {
-      body.style.overflow = ''; 
-    }
-  } 
+  if (hamburger) {
+      document.addEventListener('click', function(event) {
+          // Check if the click was on the hamburger icon
+          if (event.target === hamburger) {
+              navMenu.classList.toggle('show');
+              if (navMenu.classList.contains('show')) {
+                  body.style.overflow = 'hidden';
+              } else {
+                  body.style.overflow = ''; 
+              }
+          } 
 
-  // Check if the clicked element is outside the nav menu and hamburger
-  else if (!navMenu.contains(event.target) && !hamburger.contains(event.target)) {
-    navMenu.classList.remove('show');
-    body.style.overflow = ''; 
+          // Check if the clicked element is outside the nav menu and hamburger
+          else if (navMenu && !navMenu.contains(event.target) && !hamburger.contains(event.target)) {
+              navMenu.classList.remove('show');
+              body.style.overflow = ''; 
+          }
+      });
   }
 });
